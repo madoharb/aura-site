103586ad6a6290ef64f5d63c1b7db1be123a9b4e
Mado Harb <12433133+madoharb@users.noreply.github.com>
2022-04-22 07:44:03 +0300

    v11

--- components/header.js
+++ components/header.js
@@ -87,12 +87,12 @@ const Header = ({ data = {}, isTransparent, onSetup = () => { } }) => {
                                         aria-label="Go Home"
                                         onClick={() => window.scrollTo(0, 0)}
                                     >
-                                        <img src="/HULL-Logo.svg" />
+                                        <img src="/Vibe-1.png" />
                                     </button>
                                 ) : (
                                         <Link href="/" scroll={false}>
                                             <a className="logo--link" aria-label="Go Home">
-                                                <img src="/HULL-Logo.svg" />
+                                                <img src="/Vibe-1.png" />
                                             </a>
                                         </Link>
                                     )}
